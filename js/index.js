6a2db2fd0afbb57f0c8261d2611c4c6c1fd14172
ia6599 <ia6599@qq.com>
2020-11-25 17:28:58 +0800

    2020-11-25

--- js/index.js
+++ js/index.js
@@ -1,5 +1,14 @@
 $(function() {
     $(document).ready(function() {
-        $('#fullpage').fullpage();
+        $("#fullpage").fullpage();
     });
+    $('.music-icon').click(function() {
+        if ($(this).hasClass('play')) {
+            $('.musicCon')[0].play()
+            $(this).removeClass('play')
+        } else {
+            $('.musicCon')[0].pause()
+            $(this).addClass('play')
+        }
+    })
 });
