0dc3aaea5e13802bbc19b9550c7fb0a40340fe9e
ia6599 <ia6599@qq.com>
2020-12-03 17:15:13 +0800

    2020-12-3

--- server/addNameList.php
+++ server/addNameList.php
@@ -13,20 +13,22 @@ mysqli_set_charset($link, 'utf8');
 $selectSql = "select * from name_list";
 $selectQry = mysqli_query($link, $selectSql);
 $selectRows = mysqli_fetch_all($selectQry, MYSQLI_ASSOC);
+
 for ($i = 0; $i <count($selectRows); $i++) {
     if ($selectRows[$i]['name'] == $name && $selectRows[$i]['tell'] == $tell) {
         $isUpdate = true;
     break;
     }
 }
+
 if ($isUpdate) {
     $updateSql = "update name_list set date='$date', car='$carNum',arrive='$arrive' where name='$name' and tell='$tell'";
     $updateQry = mysqli_query($link,$updateSql);
-    var_dump($updateQry);
 } else {
     $sql = "insert into name_list (name,tell,date,car,arrive) values ('$name','$tell','$date','$carNum','$arrive')";
     $qry = mysqli_query($link,$sql);
 }
+
 if ($greeting) {
     $sql_1 = "insert into greeting_list (name,greeting) values ('$name','$greeting')";
     $qry_1 = mysqli_query($link,$sql_1);
@@ -34,7 +36,7 @@ if ($greeting) {
     $qry_1 = true;
 }
 
-if ($qry && $qry_1) {
+if (($qry || $updateQry) && $qry_1) {
     $json = json_encode(array(
         "status"=>200,
         "message"=>"添加成功！"
